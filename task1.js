0d6ad083401cb76b2607fb11581a305f55b8156f
Anna-Chumakova <ann020187@gmail.com>
2023-03-11 23:18:49 +0200

    task about Walk(array)

---
+++ task1.js
@@ -0,0 +1,24 @@
+// function isValidWalk(walk) {
+//     if (walk.length === 10) {
+//         console.log(true)
+//         return true;
+//     } else {
+//         console.log(false)
+//         return false;
+//     }   
+// }
+
+function isValidWalk(walk) {
+    let valid;
+    for (let i = 0; i <= walk.length; i += 1) {
+        valid = walk.length === 10 && walk[i] !== walk[i + 1] && walk[1] === walk[9] 
+        const result = valid ? true : false
+        console.log(result)
+        return  result  
+    }
+}
+
+isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']);
+isValidWalk(['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e']);
+isValidWalk(['w']);
+isValidWalk(['n','n','n','s','n','s','n','s','n','s'])
