0a3e1dedd641f71cfcdb88febcf3e2d1a77a0bbd
Anna-Chumakova <ann020187@gmail.com>
2023-03-19 20:36:22 +0200

    update task7

--- tasks7.js
+++ tasks7.js
@@ -1,10 +1,26 @@
-function solution(number) {
-    let result = 0;
+//If we list all the natural numbers below 
+//10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-    for (let i = 0; i < number; i += 1  ) {
-        if (number[i] % 3 === 0 || number[i] % 5 === 0) {
-            result += number[i];
+//Finish the solution so that it returns the sum of all the multiples 
+//of 3 or 5 below the number passed in.Additionally, 
+//if the number is negative, return 0(for languages that do have them).
+
+//Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number) {
+    if (number < 0) {
+        return 0;
+    }
+    let result = 0;
+    for (let i = 0; i < number; i += 1) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i)
+            result += i
         }
     }
+    console.log(result)
     return result;
 }
+
+solution(10)
